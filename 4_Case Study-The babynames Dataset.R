b53ed2cc2f6e84911b2dda16869b0ef41917af92
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-13 18:53:51 -0800

    added code to 4_Case file

---
+++ 4_Case Study-The babynames Dataset.R
@@ -0,0 +1,74 @@
+# DATA SET NOT LOADED HERE SO CODE WON'T WORK
+
+babynames %>%
+  # Filter for the year 1990
+  filter(year == 1990) %>%
+  # Sort the number column in descending order 
+  arrange(desc(number))
+
+babynames %>%
+  # Find the most common name in each year
+  group_by(year) %>%
+  top_n(1, number)
+
+selected_names <- babynames %>%
+  # Filter for the names Steven, Thomas, and Matthew 
+  filter(name %in% c("Steven", "Thomas", "Matthew"))
+
+# load dplyr, tidyverse, and ggplot2 packages
+# Plot the names using a different color for each name
+ggplot(selected_names, aes(x = year, y = number, color = name)) +
+  geom_line()
+
+
+# Calculate the fraction of people born each year with the same name
+babynames %>%
+  group_by(year) %>%
+  mutate(year_total = sum(number)) %>%
+  ungroup() %>%
+  mutate(fraction = number/year_total)
+  # Find the year each name is most common
+  group_by(name) %>%
+  top_n(1, fraction)
+
+
+babynames %>%
+  # Add columns name_total and name_max for each name
+  group_by(name) %>%
+  mutate(name_total = sum(number),
+         name_max = max(number)) %>%
+  # Ungroup the table 
+  ungroup() %>%
+  # Add the fraction_max column containing the number by the name maximum 
+  mutate(fraction_max = number / name_max)
+
+
+names_filtered <- names_normalized %>%
+  # Filter for the names Steven, Thomas, and Matthew
+  filter(name %in% c("Steven", "Thomas", "Matthew"))
+
+# Visualize these names over time
+ggplot(names_filtered, aes(x = year, y = fraction_max, color = name)) +
+  geom_line()
+
+
+# window function takes a vector and returns another vector of the same length
+# lag(a_vector) moves each element to the right one; the first element becomes NA and the original last element is no longer there
+# useful for comparing steps and calculating changes, e.g. seeing changes in popularity of a name over consecutive years
+
+babynames_fraction %>%
+  # Arrange the data in order of name, then year 
+  arrange(name, year) %>%
+  # Group the data by name so that mutate() works on each name
+  group_by(name) %>%
+  # Add a ratio column that contains the ratio of fraction between each year 
+  mutate(ratio = fraction / lag(fraction))
+
+
+babynames_ratios_filtered %>%
+  # Extract the largest ratio from each name 
+  top_n(1, ratio) %>%
+  # Sort the ratio column in descending order 
+  arrange(desc(ratio)) %>%
+  # Filter for fractions greater than or equal to 0.001
+  filter(fraction >= 0.001)
